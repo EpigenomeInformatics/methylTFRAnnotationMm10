c68763caf118f11ec7bf5e7cfab510122c79fd0e
“igunduz” <irembgunduz@gmail.com>
2026-08-21 22:40:46 +0200

    clean, fix warning

--- R/getGCfreq.R
+++ R/getGCfreq.R
@@ -5,9 +5,26 @@
 #' @import GenomicRanges
 #' @importFrom utils packageVersion
 #' @examples
-#' \dontrun{
-#' x <- getGCfreq()
-#' }
+#' # 1. Create a dummy object and save it to a temporary directory 
+#' # to bypass AnnotationHub during package checks.
+#' mock_dir <- tempdir()
+#' mock_file <- file.path(mock_dir, "altius_motif_gcfreq.rds")
+#' 
+#' # (Mocking a 5-row matrix as expected by the function output)
+#' mock_data <- list(mock_motif_1 = matrix(0.25, nrow = 5, ncol = 4))
+#' saveRDS(mock_data, mock_file)
+#' 
+#' # 2. Temporarily point the package's local directory to tempdir()
+#' old_opt <- getOption("methylTFRAnnotationMm10.datadir")
+#' options(methylTFRAnnotationMm10.datadir = mock_dir)
+#' 
+#' # 3. Run the function (it will now read the mock file)
+#' x <- getGCfreq(motifSet = "altius")
+#' 
+#' # 4. Restore options and clean up the temporary file
+#' options(methylTFRAnnotationMm10.datadir = old_opt)
+#' file.remove(mock_file)
+#' 
 #' @export
 getGCfreq <- function(motifSet = "altius") {
     motifSet <- .check_motif_set(motifSet)
