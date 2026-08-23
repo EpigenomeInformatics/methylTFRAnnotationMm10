299342ec22c68e258945328bfea0370652670ccd
“igunduz” <irembgunduz@gmail.com>
2026-08-23 14:38:58 +0200

    style package

--- R/getTFbindsites.R
+++ R/getTFbindsites.R
@@ -8,26 +8,26 @@
 #' # 1. Create a dummy object and save it to a temp directory
 #' mock_dir <- tempdir()
 #' mock_file <- file.path(mock_dir, "altius_tf_bindsites.rds")
-#' 
+#'
 #' # Mocking a GRangesList as expected by the function output
 #' mock_grl <- GenomicRanges::GRangesList(
-#'     mock_motif_1 = GenomicRanges::GRanges("chr1:100-200")
+#'   mock_motif_1 = GenomicRanges::GRanges("chr1:100-200")
 #' )
 #' saveRDS(mock_grl, mock_file)
-#' 
+#'
 #' # 2. Point the local directory option to the temp directory
 #' old_opt <- getOption("methylTFRAnnotationMm10.datadir")
 #' options(methylTFRAnnotationMm10.datadir = mock_dir)
-#' 
+#'
 #' # 3. Run the function
 #' x <- getTFbindsites()
-#' 
+#'
 #' # 4. Restore options and clean up
 #' options(methylTFRAnnotationMm10.datadir = old_opt)
 #' file.remove(mock_file)
-#' 
+#'
 #' @export
 getTFbindsites <- function(motifSet = "altius") {
-    motifSet <- .check_motif_set(motifSet)
-    .resolve_resource(paste0(motifSet, "_tf_bindsites.rds"))
+  motifSet <- .check_motif_set(motifSet)
+  .resolve_resource(paste0(motifSet, "_tf_bindsites.rds"))
 }
